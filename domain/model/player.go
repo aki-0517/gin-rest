dcd83bc1d1983c91204a15e2584283a947e5d576
aki-0517 <akki.pp.777@gmail.com>
2023-08-15 18:42:30 +0900

    restのApi実装(wip)

--- domain/model/player.go
+++ domain/model/player.go
@@ -1,11 +1,17 @@
 package model
 
 type Player struct {
-	PlayerID string
+	ID string
 	Name string
 	SailNo int
 	Sex string
 	IconUrl string
 	Point int
 	Comment string
+}
+
+func NewPlayer(name string) *Player {
+	return &Player{
+		Name: name,
+	}
 }
